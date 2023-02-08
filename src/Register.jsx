dbcc58fc8132727f27bc567ed848388426f9717c
rkg5142 <68627821+rkg5142@users.noreply.github.com>
2023-02-07 20:38:55 -0500

    added backend server and database integrations

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,28 +1,78 @@
 import React, {useState} from "react";
+import { Form, Button } from "react-bootstrap";
+import axios from "axios";
+
 
 export const Register = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
-    const [name, setName] = useState('');
+    const [register, setRegister] = useState(false);
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
+        const configuration = {
+            method: "post",
+            url: "localhost:3000/register",
+            data: {
+              email,
+              pass,
+            },
+          };
+    axios(configuration)
+    // make the API call
+    .then((result) => {
+    setRegister(true);
+    })
+    .catch((error) => {
+        error = new Error();
+    });
+};
+  
+        
 
-    }
+    
     return (
         <div className="auth-form-container">
             <h2>Register</h2>
-            <form className="register-form" onSubmit={handleSubmit}>
-                <label>Full Name</label>
-                <input value={name} name="name" id="name" placeholder="Full Name" />
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter Email" id="email" name="email" />
-                <label htmlFor="email">password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)}input type="password" placeholder="Enter Password" id="password" name="password" />
-                <button type="submit">Login</button>
-            </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('login')}> If you do not have an account, register here.</button>
+            <Form onSubmit={(e) => handleSubmit(e)}>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter Email"
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        value={pass}
+                        onChange={(e) => setPass(e.target.value)}
+                        placeholder="Enter Password"
+                    />
+                </Form.Group>
+    
+                <Button 
+                    variant="primary"
+                    type="submit"
+                    onClick={(e) => handleSubmit(e)}
+                >
+                    Register
+                </Button>
+
+                {register ? (
+                    <p className="text-success">You Are Registered Successfully</p>
+                    ) : (
+                    <p className="text-danger">You Are Not Registered</p>
+                    )}
+            </Form>
+        <Button className="link-btn" onClick={() => props.onFormSwitch('login')}> If you already have an account, login here.</Button>
     </div>
-    )
+    );
 }
